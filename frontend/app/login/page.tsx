0cfd2195e5df538c09c130a5d53b14f25bd98786
Quentin <q.lebez@gmail.com>
2025-01-14 11:50:59 +0100

    Initial frontend commit

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,9 @@
+import InputForm from "@/components/login/loginForm";
+
+export default function Login() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center bg-blue-950">
+            <InputForm />
+        </div>
+    );
+}
