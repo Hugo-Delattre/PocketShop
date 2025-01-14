4c63356b843031240c8f6a515796ec0a8666b282
Quentin <q.lebez@gmail.com>
2025-01-14 16:19:05 +0100

    remove useless components

--- frontend/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import InputForm from "@/components/login/loginForm";
-
-export default function Login() {
-    return (
-        <div className="w-full h-screen flex items-center justify-center bg-blue-950">
-            <InputForm />
-        </div>
-    );
-}
