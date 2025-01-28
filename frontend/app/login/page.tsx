abfa1181a785dca6b2c38473b18f555beac13da8
louis lasserre <louis.lasserre@mmibordeaux.com>
2025-01-28 11:11:37 +0100

    fix merge issue

--- frontend/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "./LoginForm";
-
-export default function Login() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center bg-blue-950">
-      <LoginForm />
-    </div>
-  );
-}
